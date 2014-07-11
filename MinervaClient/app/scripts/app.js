2c9f12c4bb9bae2205ef1db56e82218d5007482e
Cameron <cameronfletcher92@gmail.com>
2014-07-12 01:44:49 +1000

    added frontend

---
+++ MinervaClient/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name minervaApp
+ * @description
+ * # minervaApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('minervaApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ MinervaClient/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name minervaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the minervaApp
+ */
+angular.module('minervaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
