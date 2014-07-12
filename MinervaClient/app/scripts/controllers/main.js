829d8bfb7c7890013f7d36b9edfdf4e46e81ece9
Cameron <cameronfletcher92@gmail.com>
2014-07-12 14:57:28 +1000

    added a wired up kendo grid

--- MinervaClient/app/scripts/controllers/main.js
+++ MinervaClient/app/scripts/controllers/main.js
@@ -9,9 +9,76 @@
  */
 angular.module('minervaApp')
     .controller('MainCtrl', function ($scope) {
-        $scope.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];
+        $scope.gridOptions = {
+            dataSource: {
+                type: 'odata',
+                transport: {
+                    read: 'http://demos.telerik.com/kendo-ui/service/Northwind.svc/Orders'
+                },
+
+                // the schema allows for the drop-down filters to have types
+                schema: {
+                    /*
+                    data: function(data) {
+                        return data.value;
+                    },
+                    total: function(data) {
+                        return data['odata.count'];
+                    },
+                    */
+                    model: {
+                        fields: {
+                            OrderID: { type: 'number' },
+                            Freight: { type: 'number' },
+                            ShipName: { type: 'string' },
+                            OrderDate: { type: 'date' },
+                            ShipCity: { type: 'string' }
+                        }
+                    }
+                },
+
+                // server side operations
+                pageSize: 20,
+                serverPaging: true,
+                serverFiltering: true,
+                serverSorting: true
+            },
+
+            // enable virtual scrolling
+            scrollable: {
+                virtual: true
+            },
+
+            // grid settings
+            height: 550,
+            filterable: true,
+            sortable: true,
+            pageable: true,
+            groupable: true,
+
+            // column definitions for other titles and properties
+            columns: [
+                {
+                    field: 'OrderID',
+                    filterable: false
+                },
+                {
+                    field: 'Freight'
+                },
+                {
+                    field: 'OrderDate',
+                    title: 'Order Date',
+                    format: '{0:MM/dd/yyyy}'
+                },
+                {
+                    field: 'ShipName',
+                    title: 'Ship Name'
+                },
+                {
+                    field: 'ShipCity',
+                    title: 'Ship City'
+                }
+            ]
+        };
+
     });
