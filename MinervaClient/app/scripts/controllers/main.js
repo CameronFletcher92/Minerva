49ef6911ee6b8ecf6c4737328273c1260a77f157
Cameron <cameronfletcher92@gmail.com>
2014-07-18 17:25:47 +1000

    formatted the dates

--- MinervaClient/app/scripts/controllers/main.js
+++ MinervaClient/app/scripts/controllers/main.js
@@ -7,135 +7,133 @@
  * # MainCtrl
  * Controller of the minervaApp
  */
-angular.module('minervaApp')
-    .controller('MainCtrl', function ($scope) {
-        // the downtime event url
-        var dtUrl = 'http://MinervaService.cloudapp.net/api/DowntimeEvent';
-        var equipUrl = 'http://MinervaService.cloudapp.net/api/Equipment';
+angular.module('minervaApp').controller('MainCtrl', function ($scope) {
+    // the downtime event url
+    var dtUrl = 'http://MinervaService.cloudapp.net/api/DowntimeEvent';
+    var equipUrl = 'http://MinervaService.cloudapp.net/api/Equipment';
 
-        // the kendo data source for downtime events
-        var eventDataSource = new kendo.data.DataSource({
-            type: 'odata',
-            // connect to the odata controller
-            transport: {
-                read: {
-                    url: dtUrl + '?$expand=Equipment',
-                    dataType: 'json'
-                },
-                create: {
-                    url: dtUrl,
-                    type: "POST",
-                    dataType: "json"
-                },
-                update: {
-                    url: function (data) {
-                        return dtUrl + '(' + data['Id'] + ')';
-                    },
-                    type: "PUT",
-                    dataType: "json"
-                },
-                destroy: {
-                    url: function (data) {
-                        return dtUrl + '(' + data['Id'] + ')';
-                    },
-                    type: "DELETE",
-                    dataType: "json"
-                }
+    // the kendo data source for downtime events
+    var eventDataSource = new kendo.data.DataSource({
+        type: 'odata',
+        // connect to the odata controller
+        transport: {
+            read: {
+                url: dtUrl + '?$expand=Equipment',
+                dataType: 'json'
             },
-
-            // the schema allows for the drop-down filters to have types
-            schema: {
-                data: function(data) {
-                    return data.value;
-                },
-                total: function(data) {
-                    return data['odata.count'];
-                },
-                model: {
-                    id : "Id",
-                    fields: {
-                        "Equipment.Code" : {type:'string', editable: false},
-                        Start : {type: 'date'},
-                        End : {type: 'date'}
-                    }
-                }
+            create: {
+                url: dtUrl,
+                type: "POST",
+                dataType: "json"
             },
-
-            // server side operations
-            pageSize: 20,
-            serverPaging: true,
-            serverFiltering: true,
-            serverSorting: true
-        });
-
-        // the kendo hierarchical data source for equipments
-        var equipDataSource = new kendo.data.HierarchicalDataSource({
-            type: 'odata',
-            // connect to the odata controller
-            transport: {
-                read: {
-                    url: equipUrl,
-                    dataType: 'json'
-                }
+            update: {
+                url: function (data) {
+                    return dtUrl + '(' + data['Id'] + ')';
+                },
+                type: "PUT",
+                dataType: "json"
             },
-            schema: {
-                data: function(data) {
-                    return data.value;
+            destroy: {
+                url: function (data) {
+                    return dtUrl + '(' + data['Id'] + ')';
                 },
-                total: function(data) {
-                    return data['odata.count'];
-                },
-                model: {
-                    id : "Id",
-                    fields: {
-                        Code : {type:'string', editable: false},
-                        Description : {type: 'string'}
-                    }
+                type: "DELETE",
+                dataType: "json"
+            }
+        },
+
+        // the schema allows for the drop-down filters to have types
+        schema: {
+            data: function (data) {
+                return data.value;
+            },
+            total: function (data) {
+                return data['odata.count'];
+            },
+            model: {
+                id: "Id",
+                fields: {
+                    "Equipment.Code": {type: 'string', editable: false},
+                    Start: {type: 'date'},
+                    End: {type: 'date'}
                 }
             }
-        });
+        },
 
-        // the options of the main grid
-        $scope.gridOptions = {
-            dataSource: eventDataSource,
-
-            // grid settings
-            height: 600,
-            filterable: true,
-            sortable: true,
-            pageable: true,
-            //groupable: true,
-
-            // column definitions for other titles and properties
-            columns: [
-                {
-                    field: 'Equipment.Code',
-                    title: 'Equipment Code'
-                },
-                {
-                    field: 'Start',
-                    title: 'Start'
-                },
-                {
-                    field: 'End',
-                    title: 'End'
-                },
-
-                // the controls
-                { command: ["edit", "destroy"], title: "&nbsp;", width: "200px" }
-            ],
-
-            // edit style
-            editable: "popup"
-        };
-
-        $scope.treeOptions = {
-            dataSource: equipDataSource,
-            template: '{{dataItem.Code}}',
-            change: function(e) {
-                console.log("Change", this.select());
-            }
-        };
-
-
+        // server side operations
+        pageSize: 20,
+        serverPaging: true,
+        serverFiltering: true,
+        serverSorting: true
     });
+
+    // the kendo hierarchical data source for equipments
+    var equipDataSource = new kendo.data.HierarchicalDataSource({
+        type: 'odata',
+        // connect to the odata controller
+        transport: {
+            read: {
+                url: equipUrl,
+                dataType: 'json'
+            }
+        },
+        schema: {
+            data: function (data) {
+                return data.value;
+            },
+            total: function (data) {
+                return data['odata.count'];
+            },
+            model: {
+                id: "Id",
+                fields: {
+                    Code: {type: 'string', editable: false},
+                    Description: {type: 'string'}
+                }
+            }
+        }
+    });
+
+    // the options of the main grid
+    $scope.gridOptions = {
+        dataSource: eventDataSource,
+
+        // grid settings
+        height: 600,
+        filterable: true,
+        sortable: true,
+        pageable: true,
+        //groupable: true,
+
+        // column definitions for other titles and properties
+        columns: [
+            {
+                field: 'Equipment.Code',
+                title: 'Equipment Code'
+            },
+            {
+                field: 'Start',
+                title: 'Start',
+                format: '{0: yyyy-MM-dd HH:mm}'
+            },
+            {
+                field: 'End',
+                title: 'End',
+                format: '{0: yyyy-MM-dd HH:mm}'
+            },
+
+            // the controls
+            { command: ["edit", "destroy"], title: "&nbsp;", width: "200px" }
+        ],
+
+        // edit style
+        editable: "popup"
+    };
+
+    $scope.treeOptions = {
+        dataSource: equipDataSource,
+        template: '{{dataItem.Code}}'
+    };
+
+
+});
