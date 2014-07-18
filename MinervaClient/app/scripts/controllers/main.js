191f854d9263f85ead8222fa06a15b55c31e91cd
Cameron <cameronfletcher92@gmail.com>
2014-07-18 17:09:12 +1000

    basic equipment loading happening

--- MinervaClient/app/scripts/controllers/main.js
+++ MinervaClient/app/scripts/controllers/main.js
@@ -11,73 +11,96 @@ angular.module('minervaApp')
     .controller('MainCtrl', function ($scope) {
         // the downtime event url
         var dtUrl = 'http://MinervaService.cloudapp.net/api/DowntimeEvent';
+        var equipUrl = 'http://MinervaService.cloudapp.net/api/Equipment';
+
+        // the kendo data source for downtime events
+        var eventDataSource = new kendo.data.DataSource({
+            type: 'odata',
+            // connect to the odata controller
+            transport: {
+                read: {
+                    url: dtUrl + '?$expand=Equipment',
+                    dataType: 'json'
+                },
+                create: {
+                    url: dtUrl,
+                    type: "POST",
+                    dataType: "json"
+                },
+                update: {
+                    url: function (data) {
+                        return dtUrl + '(' + data['Id'] + ')';
+                    },
+                    type: "PUT",
+                    dataType: "json"
+                },
+                destroy: {
+                    url: function (data) {
+                        return dtUrl + '(' + data['Id'] + ')';
+                    },
+                    type: "DELETE",
+                    dataType: "json"
+                }
+            },
+
+            // the schema allows for the drop-down filters to have types
+            schema: {
+                data: function(data) {
+                    return data.value;
+                },
+                total: function(data) {
+                    return data['odata.count'];
+                },
+                model: {
+                    id : "Id",
+                    fields: {
+                        "Equipment.Code" : {type:'string', editable: false},
+                        Start : {type: 'date'},
+                        End : {type: 'date'}
+                    }
+                }
+            },
+
+            // server side operations
+            pageSize: 20,
+            serverPaging: true,
+            serverFiltering: true,
+            serverSorting: true
+        });
+
+        // the kendo hierarchical data source for equipments
+        var equipDataSource = new kendo.data.HierarchicalDataSource({
+            type: 'odata',
+            // connect to the odata controller
+            transport: {
+                read: {
+                    url: equipUrl,
+                    dataType: 'json'
+                }
+            },
+            schema: {
+                data: function(data) {
+                    return data.value;
+                },
+                total: function(data) {
+                    return data['odata.count'];
+                },
+                model: {
+                    id : "Id",
+                    fields: {
+                        Code : {type:'string', editable: false},
+                        Description : {type: 'string'}
+                    }
+                }
+            }
+        });
 
         // the options of the main grid
         $scope.gridOptions = {
-
-            dataSource: {
-                type: 'odata',
-                // connect to the odata controller
-                transport: {
-                    read: {
-                        url: dtUrl + '?$expand=Equipment',
-                        dataType: 'json'
-                    },
-                    create: {
-                        url: dtUrl,
-                        type: "POST",
-                        dataType: "json"
-                    },
-                    update: {
-                        url: function (data) {
-                            return dtUrl + '(' + data['Id'] + ')';
-                        },
-                        type: "PUT",
-                        dataType: "json"
-                    },
-                    destroy: {
-                        url: function (data) {
-                            return dtUrl + '(' + data['Id'] + ')';
-                        },
-                        type: "DELETE",
-                        dataType: "json"
-                    }
-                },
-
-                // the schema allows for the drop-down filters to have types
-                schema: {
-                    data: function(data) {
-                        return data.value;
-                    },
-                    total: function(data) {
-                        return data['odata.count'];
-                    },
-                    model: {
-                        id : "Id",
-                        fields: {
-                            "Equipment.Code" : {type:'string', editable: false},
-                            Start : {type: 'date'},
-                            End : {type: 'date'}
-                        }
-                    }
-                },
-
-                // server side operations
-                pageSize: 20,
-                serverPaging: true,
-                serverFiltering: true,
-                serverSorting: true
-            },
-
-            // enable virtual scrolling
-            /*
-            scrollable: {
-                virtual: true
-            },
-            */
+            dataSource: eventDataSource,
 
             // grid settings
-            height: 550,
+            height: 600,
             filterable: true,
             sortable: true,
             pageable: true,
@@ -102,9 +125,16 @@ angular.module('minervaApp')
                 { command: ["edit", "destroy"], title: "&nbsp;", width: "200px" }
             ],
 
-            // the controls
-            //toolbar: ["create"],
+            // edit style
             editable: "popup"
+        };
+
+        $scope.treeOptions = {
+            dataSource: equipDataSource,
+            template: '{{dataItem.Code}}',
+            change: function(e) {
+                console.log("Change", this.select());
+            }
         };
 
 
